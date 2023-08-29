33ca455fa0cb6fb6090ebe7bb096491b3a6c214b
Laye Mehta <layeamehta@gmail.com>
2023-08-28 20:38:48 -0600

    initial spring boot commit

---
+++ src/main/kotlin/com/connectly/messengerreviewbot/MessengerReviewBotApplication.kt
@@ -0,0 +1,11 @@
+package com.connectly.messengerreviewbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessengerReviewBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MessengerReviewBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/connectly/messengerreviewbot/MessengerReviewBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.connectly.messengerreviewbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessengerReviewBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
