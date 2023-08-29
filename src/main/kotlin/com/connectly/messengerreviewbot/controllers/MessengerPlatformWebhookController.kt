341e4d52e3acbb41974b43f642927d71534f7b1a
Laye Mehta <layeamehta@gmail.com>
2023-08-28 21:43:32 -0600

    boilerplate messenger platform verification request endpoint

--- src/main/kotlin/com/connectly/messengerreviewbot/controllers/MessengerPlatformWebhookController.kt
+++ src/main/kotlin/com/connectly/messengerreviewbot/controllers/MessengerPlatformWebhookController.kt
@@ -3,17 +3,31 @@ package com.connectly.messengerreviewbot.controllers
 import com.connectly.messengerreviewbot.controllers.models.IncomingMessageEvent
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/api/v1/messenger-platform-webhook")
 class MessengerPlatformWebhookController() {
 
     @PostMapping
-    fun receiveMessengerPlatformEvent(@RequestBody body: IncomingMessageEvent): ResponseEntity<Any> {
+    fun receiveMessengerPlatformWebhookEvent(@RequestBody body: IncomingMessageEvent): ResponseEntity<Any> {
         println(body)
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping
+    fun messengerPlatformWebhookVerificationRequest(
+        @RequestParam("hub.mode") hubMode: String?,
+        @RequestParam("hub.verify_token") verifyToken: String?,
+        @RequestParam("hub.challenge") challenge: String?
+    ): ResponseEntity<Any> {
+        if(hubMode != null && verifyToken != null) {
+            if(hubMode == "subscribe" && verifyToken == "hi") return ResponseEntity.ok(challenge)
+        }
         return ResponseEntity.ok().build()
     }
 
