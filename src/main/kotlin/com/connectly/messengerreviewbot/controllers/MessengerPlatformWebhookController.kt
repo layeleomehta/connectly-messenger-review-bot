983515c9a50154b37c37bed28adc83b76e996893
Laye Mehta <layeamehta@gmail.com>
2023-08-28 21:53:14 -0600

    using env config for verify token; changed incoming time data type to Instant

--- src/main/kotlin/com/connectly/messengerreviewbot/controllers/MessengerPlatformWebhookController.kt
+++ src/main/kotlin/com/connectly/messengerreviewbot/controllers/MessengerPlatformWebhookController.kt
@@ -1,6 +1,7 @@
 package com.connectly.messengerreviewbot.controllers
 
 import com.connectly.messengerreviewbot.controllers.models.IncomingMessageEvent
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/api/v1/messenger-platform-webhook")
-class MessengerPlatformWebhookController() {
+class MessengerPlatformWebhookController(
+    @Value("\${connectly.messenger-platform.webhook.verify-token}")
+    private val messengerPlatformWebhookVerifyToken: String
+) {
 
     @PostMapping
     fun receiveMessengerPlatformWebhookEvent(@RequestBody body: IncomingMessageEvent): ResponseEntity<Any> {
@@ -26,7 +30,7 @@ class MessengerPlatformWebhookController() {
         @RequestParam("hub.challenge") challenge: String?
     ): ResponseEntity<Any> {
         if(hubMode != null && verifyToken != null) {
-            if(hubMode == "subscribe" && verifyToken == "hi") return ResponseEntity.ok(challenge)
+            if(hubMode == "subscribe" && verifyToken == messengerPlatformWebhookVerifyToken) return ResponseEntity.ok(challenge)
         }
         return ResponseEntity.ok().build()
     }
--- src/main/kotlin/com/connectly/messengerreviewbot/controllers/models/IncomingMessageEvent.kt
+++ src/main/kotlin/com/connectly/messengerreviewbot/controllers/models/IncomingMessageEvent.kt
@@ -1,6 +1,7 @@
 package com.connectly.messengerreviewbot.controllers.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.Instant
 
 data class IncomingMessageEvent(
     @JsonProperty("object")
@@ -10,14 +11,14 @@ data class IncomingMessageEvent(
 
 data class Entry(
     val id: String,
-    val time: Int,
+    val time: Instant,
     val messaging: List<Messaging>
 )
 
 data class Messaging(
     val sender: Sender,
     val recipient: Recipient,
-    val timestamp: Int,
+    val timestamp: Instant,
     val message: Message
 )
 
@@ -33,27 +34,3 @@ data class Message(
     val mid: String,
     val text: String
 )
-//{
-//    "object": "page",
-//    "entry": [
-//    {
-//        "id": "112810605251603",
-//        "time": 1693273305347,
-//        "messaging": [
-//        {
-//            "sender": {
-//            "id": "6456535891062838"
-//        },
-//            "recipient": {
-//            "id": "112810605251603"
-//        },
-//            "timestamp": 1693273305043,
-//            "message": {
-//            "mid": "m_kIOH_0L_3BNAcXkPz1yFK6RrcqT1Hk_0J6AskfsiOUPnYioxxSxzH2_KTxrwPjUi10DV8zITRLcNdGd-z4w7kA",
-//            "text": "laye is cool"
-//        }
-//        }
-//        ]
-//    }
-//    ]
-
