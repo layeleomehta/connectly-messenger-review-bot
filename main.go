de4c440100388ba433c9689d91c614d646ab3c3c
Laye Mehta <layeamehta@gmail.com>
2023-08-28 19:06:09 -0600

    testing out the webhooks, able to verify and receive message events

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hi my name is laye")
+	r := gin.Default()
+
+	r.POST("/webhook", func(c *gin.Context) {
+		// Handle the message
+		c.String(http.StatusOK, c.)
+
+	})
+
+	r.GET("/webhook", func(c *gin.Context) {
+		if c.DefaultQuery("hub.verify_token", "") == "hi" {
+			c.String(http.StatusOK, c.DefaultQuery("hub.challenge", ""))
+			return
+		}
+		c.String(http.StatusForbidden, "Error, wrong validation token")
+	})
+
+	r.Run(":8080")
 }
