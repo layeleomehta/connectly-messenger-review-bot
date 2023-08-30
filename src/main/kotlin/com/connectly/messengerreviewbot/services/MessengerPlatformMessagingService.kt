bc29c975d52c32658009800aceb047f1577ceb43
Laye Mehta <layeamehta@gmail.com>
2023-08-29 19:25:12 -0600

    customer feedback POST body data class

--- src/main/kotlin/com/connectly/messengerreviewbot/services/MessengerPlatformMessagingService.kt
+++ src/main/kotlin/com/connectly/messengerreviewbot/services/MessengerPlatformMessagingService.kt
@@ -2,17 +2,18 @@ package com.connectly.messengerreviewbot.services
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
 
 @Service
 class MessengerPlatformMessagingService(
+    private val restTemplate: RestTemplate,
     @Value("\${connectly.messenger-platform.send-api.base-url}")
     private val messengerPlatformSendApiBaseUrl: String,
     @Value("\${connectly.messenger-platform.send-api.version}")
     private val messengerPlatformSendApiVersion: String
     ) {
 
-    fun sendCustomerFeedbackMessage() {
-
+    fun sendCustomerFeedbackMessage(recipientPsid: String) {
     }
 
 
