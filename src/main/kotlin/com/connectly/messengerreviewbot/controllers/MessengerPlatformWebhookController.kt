3f62707d392be5bf596d2419cae639da4483d2ec
Laye Mehta <layeamehta@gmail.com>
2023-08-29 23:15:58 -0600

    now prompting for customer review upon persistent menu postback

--- src/main/kotlin/com/connectly/messengerreviewbot/controllers/MessengerPlatformWebhookController.kt
+++ src/main/kotlin/com/connectly/messengerreviewbot/controllers/MessengerPlatformWebhookController.kt
@@ -3,6 +3,7 @@ package com.connectly.messengerreviewbot.controllers
 import com.connectly.messengerreviewbot.controllers.models.IncomingMessageEvent
 import com.connectly.messengerreviewbot.services.BusinessPageService
 import com.connectly.messengerreviewbot.services.CustomerFeedbackReviewService
+import com.connectly.messengerreviewbot.services.MessengerPlatformMessagingService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
@@ -21,7 +22,8 @@ class MessengerPlatformWebhookController(
     @Value("\${connectly.messenger-platform.send-api.review-question-id}")
     private val messengerPlatformReviewQuestionId: String,
     private val businessPageService: BusinessPageService,
-    private val customerFeedbackReviewService: CustomerFeedbackReviewService
+    private val customerFeedbackReviewService: CustomerFeedbackReviewService,
+    private val messengerPlatformMessagingService: MessengerPlatformMessagingService
 ) {
     private val logger = LoggerFactory.getLogger(MessengerPlatformWebhookController::class.java)
 
@@ -50,7 +52,13 @@ class MessengerPlatformWebhookController(
                 }
 
                 if(messaging.postback != null) {
-                    // process a message postback and prompt for a review:
+                    // process a message postback and request for a customer feedback review:
+                    if(messaging.postback.payload == "PERSISTENT_MENU_REVIEW" || messaging.postback.payload == "GET_STARTED_REVIEW") {
+                        messengerPlatformMessagingService.sendCustomerFeedbackMessage(
+                            recipientPsid = messaging.sender.id,
+                            businessPage = businessPage
+                        )
+                    }
                 }
             }
         }
