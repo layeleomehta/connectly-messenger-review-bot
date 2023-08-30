6bbdba8805a7750c3f1e9d5595049ecad6b989c0
Laye Mehta <layeamehta@gmail.com>
2023-08-29 18:59:42 -0600

    boilerplate service for sending messages; environment vars for send api

---
+++ src/main/kotlin/com/connectly/messengerreviewbot/services/MessengerPlatformMessagingService.kt
@@ -0,0 +1,15 @@
+package com.connectly.messengerreviewbot.services
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class MessengerPlatformMessagingService(
+    @Value("\${connectly.messenger-platform.send-api.base-url}")
+    private val messengerPlatformSendApiBaseUrl: String,
+    @Value("\${connectly.messenger-platform.send-api.version}")
+    private val messengerPlatformSendApiVersion: String
+    ) {
+
+
+}
